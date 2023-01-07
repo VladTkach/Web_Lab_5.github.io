9adcdfe45cc52bb7b2390de9acdda1e8178763cc
VladTkach <vladand2003@gmail.com>
2023-01-07 09:33:58 +0200

    add clear and local storage to 5

--- script_5.js
+++ script_5.js
@@ -1,3 +1,12 @@
+window.addEventListener('load', (event) => {
+    document.querySelectorAll(".list__selection").forEach((ol) =>{
+        ol.innerHTML = localStorage.getItem(ol.id.replace("_list", ""));
+        if (ol.innerHTML.toString().length > 0){
+            document.getElementById(ol.id.replace("_list", "_rbtn")).classList.add("active__btn");
+        }
+    })
+});
+
 document.querySelectorAll(".script_5").forEach((block) =>{
     block.addEventListener("mouseup", () =>{
         console.log(block.id)
@@ -18,10 +27,25 @@ function closeSelectionBtn(){
 
 document.querySelectorAll(".select__btn").forEach((btn) => {
     btn.addEventListener("click", () =>{
-        console.log("click" + btn.id.toString())
+        console.log("click" + btn.id.toString());
+        document.getElementById(btn.id.replace("_btn", "_rbtn")).classList.add("active__btn");
         var ul = document.getElementById(btn.id.replace("_btn", "_list"));
         var li = document.createElement("li");
-        li.appendChild(document.createTextNode(document.getSelection().toString()));
+        var text = document.createTextNode(document.getSelection().toString());
+        li.appendChild(text);
         ul.appendChild(li);
+        console.log(btn.id.replace("_btn", ""));
+        localStorage.setItem(btn.id.replace("_btn", ""), ul.innerHTML);
+    })
+})
+
+document.querySelectorAll(".clear__btn").forEach((btn) => {
+    btn.addEventListener("click", () =>{
+        console.log("click" + btn.id.toString());
+        var ol = document.getElementById(btn.id.replace("_rbtn", "_list"));
+        while (ol.firstChild)
+            ol.removeChild(ol.firstChild);
+        btn.classList.remove("active__btn")
+        localStorage.setItem(btn.id.replace("_rbtn", ""), ol.innerHTML);
     })
 })
